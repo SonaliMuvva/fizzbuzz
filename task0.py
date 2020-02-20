6bfa2c16d7504bff88daf67e1c0dbc110376c794
sonalimuvva <sn621173@dal.ca>
2020-02-20 18:18:39 +0000

    multiples of 3, 5, and 3,5

--- task0.py
+++ task0.py
@@ -2,12 +2,12 @@ for i in range(1,101):
     if (i%3==0):
         print("frizz")
         i+=1
-    if (i%5==0):
+    elif (i%5==0):
         print("buzz")
         i+=1
-    if (i % 3 ==0) or (i % 5 == 0) and (i % 15 != 0):
+    elif (i % 3 ==0) and (i % 5 == 0):
         print("frizzbuzz")
         i+=1
-    
-    print(i)
-    i +=1
+    else:   
+        print(i)
+        i +=1    
